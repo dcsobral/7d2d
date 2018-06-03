6afd73498ea21c3bec4bcc12c1dbb165880ef0a1
Daniel C. Sobral <dcsobral@gmail.com>
2018-06-02 20:08:17 -0600

    Add listPrefabContainers.sh script
    Produces a list of every prefab in rwg, followed by the count of
    every lootable container in it.
    
    The script is broken down in multiple components, to allow easy
    customization of output.

---
+++ listPrefabContainers.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+consoleCommand() {
+  curl  --data-urlencode "command=$1" -s --get -s "http://localhost:8082/api/executeconsolecommand?adminuser=user&admintoken=password" | jq ".result|fromjson"
+}
+
+prefabCount() {
+  xmlstarlet sel -t -m "/" -v "count(//*[@name='$1'])" -n rwgmixer.xml
+}
+
+rwgInfo() {
+  xmlstarlet sel -t -m "//*[@name='$1']" -m "ancestor::*" -v "name(.)" -m "@*" -o " " -v "name(.)" -o "=" -v "." -b -o "/" -b -v "name(.)" -m "@*" -o " " -v "name(.)" -o "=" -v "." -b -n rwgmixer.xml  
+}
+
+lootableBlocks() {
+  xmlstarlet sel -t -m / -o "[" -m "//block[property[@name='LootList']]" -o '"' -v @id -o '"' --if "not(position()=last())" -o ", " -b -b -o "]" -n blocks.xml
+}
+
+LOOTABLE_BLOCKS="$(lootableBlocks)"
+
+isLootable() {
+  jq --argjson lootable "${LOOTABLE_BLOCKS}" 'map(select(.Id as $blockId | $lootable|map(.==$blockId)|any))'
+}
+
+consoleCommand "bc-prefab list" | jq -r ".Prefabs[]" | while read prefab
+do
+  if [[ $(prefabCount "$prefab") > 0 ]]
+  then
+    echo "Prefab $prefab"
+    rwgInfo "$prefab"
+    consoleCommand "bc-prefab blockcount \"$prefab\"" | jq ".Blocks" | isLootable | jq --compact-output .[]
+  fi
+done
+
+# vim: et:ts=2:sw=2
